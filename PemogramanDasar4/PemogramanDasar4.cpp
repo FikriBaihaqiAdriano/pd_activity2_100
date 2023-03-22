0c1f6ac87023abfbcbb9fd755881b27b49fd2588
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-22 15:20:46 +0700

    prosedur menampilkan menu dan pilihan

--- PemogramanDasar4/PemogramanDasar4.cpp
+++ PemogramanDasar4/PemogramanDasar4.cpp
@@ -8,6 +8,28 @@ float Luaspersegipanjang(float p, float l) {
 float Luassegitiga(float a, float t) {
 	return 0.5 * a * t;
 }
+
 float Luaslingkaran(float r) {
 	return 3.14 * r * r;
+}
+
+
+int main() {
+
+	float panjang, lebar, tinggi, jejari, alas;
+	int pilihan;
+
+
+	cout << "____________________" << endl;
+	cout << "______M E N U_______" << endl;
+	cout << "____________________" << endl;
+
+	cout << "1. Luas Persegipanjang" << endl;
+	cout << "2. Luas Segitiga" << endl;
+	cout << "3. Luas Lingkaran" << endl;
+	cout << "4. Exit" << endl;
+
+	cout << "Masukan pilihan : ";
+	cin >> pilihan;
+
 }
