7eb6ce3296a043d5f78185b24abdf677aa70bb22
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-22 15:01:37 +0700

    Create Prosedur Luas Lingkaran

--- PemogramanDasar4/PemogramanDasar4.cpp
+++ PemogramanDasar4/PemogramanDasar4.cpp
@@ -4,6 +4,10 @@ using namespace std;
 float Luaspersegipanjang(float p, float l) {
 	return p * l;
 }
+
 float Luassegitiga(float a, float t) {
 	return 0.5 * a * t;
+}
+float Luaslingkaran(float r) {
+	return 3.14 * r * r;
 }
